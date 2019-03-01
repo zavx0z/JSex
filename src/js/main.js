0fbe8a9dbbd8dc4797b2352980c8005c40fa45a3
zavx0z <zavx0z@ya.ru>
2019-03-01 15:40:49 +0300

    одновременное разрешение всех промисов через map

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,10 @@ import {delayedLog} from "./delayedLog"
 
 const arr = [1, 2, 3]
 
-const processArray = async arr => {
-    const results = []
-    for (const item of arr)
-        results.push(await delayedLog(item))
+let processArray = async (array) => {
+    const promises = array.map(delayedLog)
+    await Promise.all(promises);
     console.info("Я тут ждал разрешения всех промисов")
-    return await Promise.all(results)
 }
 
 processArray(arr).catch((error) => console.error(error))
